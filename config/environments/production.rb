ac5b3fa1394ecf96a965cb8a5f1a5d91e7e9b3b3
stephen.rycyk <stephen.rycyk@googlemail.com>
2017-03-14 21:50:04 +0000

    Devise PRD debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
