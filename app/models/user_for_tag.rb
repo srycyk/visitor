061dce0207474b48fb222030fa09cd13d500debf
stephen.rycyk <stephen.rycyk@googlemail.com>
2017-04-09 15:45:08 +0100

    Delete user data

--- app/models/user_for_tag.rb
+++ app/models/user_for_tag.rb
@@ -13,6 +13,14 @@ module UserForTag
     validates :name, uniqueness: { scope: %i(tag_id user_id) }
   end
 
+  class_methods do
+    def rm!(user)
+      transaction do
+        user.tags.roots.each {|tag| tag.expunge }
+      end
+    end
+  end
+
   def owner?(candidate_user)
     candidate_user and to_param == candidate_user.to_param
   end
